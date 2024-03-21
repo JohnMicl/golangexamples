2318fe3b526465b6cb296ffaeefa288ae78d0b59
JohnMicl <996259211@qq.com>
2024-03-21 14:35:28 +0800

    add basic info

--- basic/main.go
+++ basic/main.go
@@ -39,6 +39,8 @@ func ChannelWithBuff() {
 
 // package sync
 func SyncPool() {
+	// sync.Pool 是 Go 语言标准库 sync 包中的一个类型，它提供了一个内存对象池的实现。
+	// sync.Pool 主要用于存储临时对象，以便在后续需要时重用这些对象，从而避免频繁的内存分配和垃圾回收带来的性能开销。
 	mypool := sync.Pool{
 		New: func() interface{} {
 			fmt.Println("creating new instance.")
@@ -49,6 +51,8 @@ func SyncPool() {
 	instance := mypool.Get()
 	mypool.Put(instance)
 	mypool.Get()
+	// sync.Pool 内部的管理是基于运行时机制的，它不保证对象的存活时间，系统可能在任何时间清理 pool 中的对象，特别是当内存压力较大时。
+	// 因此，不应该依赖 pool 存储重要的数据，尤其是那些需要长期存在的数据。同时，存入 pool 的对象应当在其生命周期结束前确保其状态可以安全地复用。
 }
 
 func SyncOnce() {
@@ -73,9 +77,14 @@ func SyncOnce() {
 }
 
 func SyncBroadcast() {
+	// sync.Cond 是 Go 语言标准库 sync 包中的一个并发原语，用于在多个 goroutine 之间进行同步和通信。
+	// 它是一个条件变量，允许 goroutine 在某个条件满足时阻塞等待，并在条件变为真时被唤醒。
 	type Button struct {
 		Clicked *sync.Cond
 	}
+
+	// 使用 sync.Cond 需要配合一个互斥锁（通常是 sync.Mutex 或 sync.RWMutex），
+	// 因为条件变量的 Wait() 方法需要在锁定状态下调用，而 Signal() 和 Broadcast() 方法则用来通知等待的 goroutine。
 	button := Button{Clicked: sync.NewCond(&sync.Mutex{})}
 	subscribe := func(c *sync.Cond, fn func()) {
 		var goroutineRunning sync.WaitGroup
@@ -105,6 +114,8 @@ func SyncBroadcast() {
 		fmt.Println("mouse window")
 		clickRegisterd.Done()
 	})
+
+	// 如果有多于一个的 goroutine 在等待同一个条件变量，可以使用 cond.Broadcast() 来唤醒所有等待的 goroutine。
 	button.Clicked.Broadcast()
 	clickRegisterd.Wait()
 }
